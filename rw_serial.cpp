53853b730f5dc17af6482f462a4a20bf93e40f30
chemaiRenny <chemairenny@gmail.com>
2022-02-09 13:19:57 +0300

    Read and Write to Serial USB

--- rw_serial.cpp
+++ rw_serial.cpp
@@ -1,8 +1,7 @@
 #include "mbed.h"
 
 #define MAXIMUM_BUFFERSIZE 512
-#define ON_CHR 'K'
-#define OFF_CHR 'D'
+#define BAUDRATE 115200
 
 DigitalOut myLed(LED1);
 BufferedSerial sp(USBTX,USBRX);
@@ -11,14 +10,15 @@ int main()
 {
     char buf[MAXIMUM_BUFFERSIZE]={0};
 
-    sp.set_baud(115200);
+    sp.set_baud(BAUDRATE); //baudrate
     while(1)
     {
-        sp.read(buf,sizeof(buf));
-        sp.write(buf,sizeof(buf));
+        
+        sp.read(buf,sizeof(buf));  //read serial Object
+        sp.write(buf,sizeof(buf)); //write to serial object
 
         myLed = !myLed;
-        //ThisThread::sleep_for(500ms);
-
+        //ThisThread::sleep_for(500ms); //no need for wait
     }
 }
+ 
